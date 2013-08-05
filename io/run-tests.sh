38a90c01d0ebbf2aa78641e10d30c80eee2d8442
Steven Lu <tacticalazn@gmail.com>
2013-08-04 23:40:50 -0400

    Making run-tests more verbose.

--- io/run-tests.sh
+++ io/run-tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for num in 0 1 2 3 4 5 6; do
    for alg in f s S C F; do
+      echo "${alg} input${num}"
       ./iosched -s${alg} -v tests/input${num} | diff -b -B -q ./tests/out${num}_${alg}_long -
    done
 done
